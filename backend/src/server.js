3e8ed1f0ab196461986caae794f7159235e07833
najwanzen <muttaqinnajwan4@gmail.com>
2025-09-24 10:59:57 +0700

    tugas-2

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+import express from "express";
+import { testConnection } from "./config/db.js"
+import helloRouter from "./routes/helloRoute.js";
+
+const app = express();
+const port = 3000;
+
+app.use("/", helloRouter)
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+    testConnection();
+})
